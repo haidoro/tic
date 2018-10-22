606e4304bab7bc498e0d054a55f996200df4994c
Tahara <hydoro-3181@itstudio.co>
2018-10-22 17:42:57 +0900

    Lifting State Up(2)

--- src/App.js
+++ src/App.js
@@ -2,25 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Square extends React.Component {
-    // TODO: remove the constructor
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
-        // TODO: use onClick={this.props.onClick}
-        // TODO: replace this.state.value with this.props.value
         return ( <
             button className = "square"
             onClick = {
-                () => this.setState({
-                    value: 'X'
-                })
-            } > {
-                this.state.value
+                () => this.props.onClick()
+            } >
+            {
+                this.props.value
             } <
             /button>
         );
@@ -35,11 +24,24 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({
+            squares: squares
+        });
+    }
+
     renderSquare(i) {
-        return <Square value = {
-            this.state.squares[i]
-        }
-        />;
+        return ( <
+            Square value = {
+                this.state.squares[i]
+            }
+            onClick = {
+                () => this.handleClick(i)
+            }
+            />
+        );
     }
 
     render() {
@@ -99,5 +101,6 @@ class Game extends React.Component {
         );
     }
 }
+
 
 export default Game
